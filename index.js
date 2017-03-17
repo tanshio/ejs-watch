6521b3e3621d2a45f5bba9f4a598124a58f78624
tanshio <rakuburomail+tanshio@gmail.com>
2017-03-18 00:28:03 +0900

    watch

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var chokidar = require('chokidar');
+var ejs = require('ejs');
 
 // One-liner for current directory, ignores .dotfies
 chokidar.watch('.', {ignored: /(^|[\/\\])\..|node_modules/}).on('all', (event, path) => {
-  console.log(event, path);
+
+  console.log(event, path)
+
+  const data = {}
+  options = {}
+  ejs.renderFile(path, data, options, function(err, str){
+    // str => Rendered HTML string
+    console.log(err);
+    console.log(str);
+  });
+
 });
